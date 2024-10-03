0e3a8d8ae3f5da97b4601f502c38a9313ad4a4b5
Poorvikabr <poorvikagowda28@gmail.com>
2024-10-03 17:19:13 +0530

    updates

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,19 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    
-    <router-view></router-view>
+    <MyPortfolio/>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-// import MyPortfolio from './components/MyPortfolio.vue';
+import MyPortfolio from './components/MyPortfolio.vue';
 
 export default {
   name: 'App',
   components: {
-  
+    // HelloWorld,
+    MyPortfolio,
   }
 }
 </script>
